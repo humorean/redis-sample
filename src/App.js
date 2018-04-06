f8b9beecaf4e34b617644677f37f0949a070ed26
Shawn Stedman <shawn.stedman@bitmex.com>
2018-04-06 10:44:46 -0700

    feat(frontend): setup frontend test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 /**
- * Create a page using React that lists the top 5 exchanges in ascending order by BTC volume given the api endpoint:
- *   http://localhost:3333/volume
+ * Create a unordered list using React that lists the top 5 exchanges by volume of currency type Bitcoin (BTC) in
+ * ascending order given the api endpoint:
+ *   http://localhost:3334/volume
  * 
- * Work the way you are most comfortable and treat this as an everyday work task. You are allowed to look up
- * documentation and search for answers.
+ * Work the way you are most comfortable and treat this as an everyday work task. You are allowed to ask questions,
+ * look up documentation and search for answers.
  */
 
 import React, { Component } from 'react';
